614501f6df3b4ca66c095833a6aeedd2e9ef8c83
Tutorials Website <46397286+tutorials-website@users.noreply.github.com>
2019-06-13 13:58:13 +0530

    routes file update
    commit rotute updated file

--- myapps/routes/index.js
+++ myapps/routes/index.js
@@ -1,16 +1,91 @@
 var express = require('express');
+var multer = require('multer');
+var path = require('path');
+var jwt = require('jsonwebtoken');
 var empModel = require('../modules/employee');
+var uploadModel = require('../modules/upload');
+
 var router = express.Router();
 var employee =empModel.find({});
+var imageData =uploadModel.find({});
 
+router.use(express.static(__dirname+"./public/"));
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+  const LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
+
+var Storage= multer.diskStorage({
+  destination:"./public/uploads/",
+  filename:(req,file,cb)=>{
+    cb(null,file.fieldname+"_"+Date.now()+path.extname(file.originalname));
+  }
+});
+
+var upload = multer({
+  storage:Storage
+}).single('file');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/upload', upload,function(req, res, next) {
+  var imageFile=req.file.filename;
+ var success =req.file.filename+ " uploaded successfully";
+
+ var imageDetails= new uploadModel({
+  imagename:imageFile
+ });
+ imageDetails.save(function(err,doc){
+if(err) throw err;
+
+imageData.exec(function(err,data){
+if(err) throw err;
+res.render('upload-file', { title: 'Upload File', records:data,   success:success });
+});
+
+ });
+
+  });
+
+
+  function checkLogin(req,res,next){
+    var myToken= localStorage.getItem('myToken');
+    try {
+     jwt.verify(myToken, 'loginToken');
+    } catch(err) {
+      res.send ("you need login to access this page");
+    }
+    next();
+  }
+
+router.get('/upload', checkLogin,function(req, res, next) {
+  imageData.exec(function(err,data){
+    if(err) throw err;
+res.render('upload-file', { title: 'Upload File', records:data, success:'' });
+  });
+});
+
+router.get('/',checkLogin,function(req, res, next) {
   employee.exec(function(err,data){
 if(err) throw err;
 res.render('index', { title: 'Employee Records', records:data, success:'' });
   });
   
 });
+router.get('/login',function(req, res, next) {
+
+  var token = jwt.sign({ foo: 'bar' }, 'loginToken');
+  localStorage.setItem('myToken', token);
+  res.send("Login Successfully");
+});
+
+router.get('/logout',function(req, res, next) {
+
+  localStorage.removeItem('myToken');
+  res.send("Logout Successfully");
+  
+});
+
+
 
 router.post('/', function(req, res, next) {
   var empDetails = new empModel({
