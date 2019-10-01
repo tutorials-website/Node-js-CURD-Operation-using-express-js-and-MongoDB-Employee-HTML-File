128ccdb8aee25a9fbe6e375f869b03e28127b8b7
Tutorials Website <46397286+tutorials-website@users.noreply.github.com>
2019-10-02 01:28:36 +0530

    Autocomplete route created
    commit autocomplete route created code

--- myapps/routes/index.js
+++ myapps/routes/index.js
@@ -207,4 +207,34 @@ employee.exec(function(err,data){
   
 });
 
+
+router.get('/autocomplete/', function(req, res, next) {
+
+  var regex= new RegExp(req.query["term"],'i');
+ 
+  var employeeFilter =empModel.find({name:regex},{'name':1}).sort({"updated_at":-1}).sort({"created_at":-1}).limit(20);
+  employeeFilter.exec(function(err,data){
+
+
+var result=[];
+if(!err){
+   if(data && data.length && data.length>0){
+     data.forEach(user=>{
+       let obj={
+         id:user._id,
+         label: user.name
+       };
+       result.push(obj);
+     });
+
+   }
+ 
+   res.jsonp(result);
+}
+
+  });
+
+});
+
+
 module.exports = router;
